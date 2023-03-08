003bc39a2c82f3712c6326387e347e3cf1ad9f4f
MoldovanMarian <moldovan.marian98@yahoo.com>
2023-03-08 15:56:43 +0200

    modificare cod

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-function adaugaObiectiv(){
-    let obiectiv = document.getElementById("obiectiv").value;
-    let prioritate = document.getElementById("prioritate").value;
+function adaugaObiectiv() {
+  let obiectiv = document.getElementById("obiectiv").value;
+  let prioritate = document.getElementById("prioritate").value;
 
-    if(obiectiv.length === 0){
-        alert("Camp gol");
-        return;
-    }else if (parseInt(prioritate) === 0){
-        alert("Camp gol");
-        return;
-    }else{
-        document.getElementById("obiectiv").value = "";
-        document.getElementById("prioritate").value = "";
-    }
+  if (obiectiv.length === 0) {
+    alert("Fill in with your objective.");
+    return;
+  } else if (parseInt(prioritate) === 0) {
+    alert("Choose one of the 3 properties.");
+    return;
+  } else {
+    document.getElementById("obiectiv").value = "";
+    document.getElementById("prioritate").value = "";
+  }
 
-    let li = document.createElement("li");
-    let text = document.createTextNode(obiectiv);
-    li.appendChild(text);
-    li.classList.add("list-group-item");
+  let li = document.createElement("li");
+  let text = document.createTextNode(obiectiv);
+  li.appendChild(text);
+  li.classList.add("list-group-item");
 
-    let lista;
+  let lista;
 
-    switch(parseInt(prioritate)){
-        case 1:
-            lista = document.getElementById("prioritate_1");
-            break;
-        case 2:
-            lista = document.getElementById("prioritate_2");
-            break;
-        case 3:
-            lista = document.getElementById("prioritate_3");
-            break;
-        default:
-            alert("Prioritate");
-            return;
-    }
+  switch (parseInt(prioritate)) {
+    case 1:
+      lista = document.getElementById("prioritate_1");
+      break;
+    case 2:
+      lista = document.getElementById("prioritate_2");
+      break;
+    case 3:
+      lista = document.getElementById("prioritate_3");
+      break;
+    default:
+      alert("Choose one of the 3 properties.");
+      return;
+  }
 
-    li.addEventListener("click", () => {
-        li.remove(li);
-})
+  li.addEventListener("click", () => {
+    li.remove(li);
+  });
 
-    lista.appendChild(li);
+  lista.appendChild(li);
 }
